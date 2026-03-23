53d36ba71608797c67d1ba4509a7de25e2ac2bab
Sidhu4533 <sidkumbhar15@gmail.com>
2026-03-23 17:59:01 +0530

    add validations to fetchListByCursor

--- handlers/fetchListByCursor.go
+++ handlers/fetchListByCursor.go
@@ -6,15 +6,46 @@ import (
 	"strconv"
 
 	"sqlitetutorial.net/go/db"
+	"sqlitetutorial.net/go/file"
 	"sqlitetutorial.net/go/models"
 )
 
 func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 
 	cid := r.URL.Query().Get("cursor")
+	if cid==""{
+		http.Error(w,"cursor Is Required",400)
+		file.FFile.Write([]byte("Limit is Required"))
+		return
+	}
 	limit := r.URL.Query().Get("limit")
-	ciid, _ := strconv.Atoi(cid)
-	l, _ := strconv.Atoi(limit)
+	if limit==""{
+		http.Error(w,"Limit Is Required",400)
+		file.FFile.Write([]byte("Limit is Required"))
+		return
+	}
+	ciid, err:= strconv.Atoi(cid)
+	if err != nil {
+		http.Error(w, "Cursor must be Valid Integer", 400)
+		file.FFile.Write([]byte(err.Error()))
+		return
+	}
+	if ciid<=0{
+		http.Error(w,"Cursor Must Be Greater Than 0",400)
+		file.FFile.Write([]byte("Cursor Must Be Greater Than 0"))
+		return
+	}
+	l, err:= strconv.Atoi(limit)
+	if err != nil {
+		http.Error(w, "Limit must be Valid Integer", 400)
+		file.FFile.Write([]byte(err.Error()))
+		return
+	}
+	if l<=0{
+		http.Error(w,"Limit Must Be Greater Than 0",400)
+		file.FFile.Write([]byte("Limit Must Be Greater Than 0"))
+		return
+	}
 
 	query := `SELECT * FROM tasks
 			WHERE Id>?
@@ -23,6 +54,7 @@ func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.DDB.Query(query, ciid, l)
 	if err != nil {
 		http.Error(w, "Database Error", 500)
+		file.FFile.Write([]byte(err.Error()))
 		return
 	}
 
@@ -36,5 +68,10 @@ func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-type", "application/json")
 	data, err := json.Marshal(tasks)
+	if err!=nil{
+		http.Error(w,"Internal Server Error",500)
+		file.FFile.Write([]byte("Internal Server Error"))
+		return
+	}
 	w.Write(data)
 }
