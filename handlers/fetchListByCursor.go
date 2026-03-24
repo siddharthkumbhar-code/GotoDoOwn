113e27ee919ec8ac9478c2a5125522942b2ea479
Sidhu4533 <sidkumbhar15@gmail.com>
2026-03-24 16:59:35 +0530

    add response next_cursor

--- handlers/fetchListByCursor.go
+++ handlers/fetchListByCursor.go
@@ -24,24 +24,24 @@ func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 		file.FFile.Write([]byte("Limit is Required"))
 		return
 	}
-	ciid, err:= strconv.Atoi(cid)
+	cursor, err:= strconv.Atoi(cid)
 	if err != nil {
 		http.Error(w, "Cursor must be Valid Integer", 400)
 		file.FFile.Write([]byte(err.Error()))
 		return
 	}
-	if ciid<=0{
+	if cursor<=0{
 		http.Error(w,"Cursor Must Be Greater Than 0",400)
 		file.FFile.Write([]byte("Cursor Must Be Greater Than 0"))
 		return
 	}
-	l, err:= strconv.Atoi(limit)
+	Limit, err:= strconv.Atoi(limit)
 	if err != nil {
 		http.Error(w, "Limit must be Valid Integer", 400)
 		file.FFile.Write([]byte(err.Error()))
 		return
 	}
-	if l<=0{
+	if Limit<=0{
 		http.Error(w,"Limit Must Be Greater Than 0",400)
 		file.FFile.Write([]byte("Limit Must Be Greater Than 0"))
 		return
@@ -51,7 +51,8 @@ func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 			WHERE Id>?
 			ORDER BY Id
 			LIMIT ? `
-	rows, err := db.DDB.Query(query, ciid, l)
+
+	rows, err := db.DDB.Query(query, cursor, Limit)
 	if err != nil {
 		http.Error(w, "Database Error", 500)
 		file.FFile.Write([]byte(err.Error()))
@@ -63,12 +64,21 @@ func FetchListByCursorPagination(w http.ResponseWriter, r *http.Request) {
 		rows.Scan(&task.Id, &task.Name, &task.Status)
 		tasks = append(tasks, task)
 	}
+
+	nextCursor:=cursor+Limit
+
+	response:=map[string]interface{}{
+		"data":tasks,
+		"next_Cursor":nextCursor,
+		"has_more":len(tasks)==Limit,
+	}
+
 	w.Header().Set("Content-type", "application/json")
-	data, err := json.Marshal(tasks)
+	responsedata, err := json.Marshal(response)
 	if err!=nil{
 		http.Error(w,"Internal Server Error",500)
 		file.FFile.Write([]byte("Internal Server Error"))
 		return
 	}
-	w.Write(data)
+	w.Write(responsedata)
 }
